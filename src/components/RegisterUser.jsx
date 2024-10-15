992783136e3779e40a8003158b1303fde4c66176
Leticia de Martino <leticiademartino@GREENWICH.local>
2024-10-15 17:03:44 +0200

    update 15.10.24

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,158 +1,176 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom';
-import "./styles.css";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import "./styles.css"
 
 export const RegisterUser = () => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [email, setEmail] = useState("");
-  const [nif, setNif] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
 
-  function handleForm(event) {
-    event.preventDefault();
+  const { register, handleSubmit, watch ,formState: { errors } } = useForm(); // aquí inicializo useForm y desestructuro errores
+  const [role, setRole] = useState("")
 
-    let userData = {
-      name,
-      lastName,
-      age,
-      email,
-      nif,
-      password,
-      role,
-    };
+    // Observamos el campo de contraseña para compararlo con la confirmación
 
-    console.log("VITE_APP_URL:", import.meta.env.VITE_APP_URL);
+  // hago una función para manerjar el envío del formulario
+
+  const onSubmit = (data) => {
+    console.log("Datos del formulario", data)
 
     fetch(`${import.meta.env.VITE_APP_URL}/user/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(userData),
+      body: JSON.stringify(data),
     })
     .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      if(!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
       }
-      return response.text(); // Cambiado a text() para verificar el contenido antes de parsear
+      return response.json(); // parseo la response a json
     })
-    .then((text) => {
-      try {
-        const data = JSON.parse(text);
-        console.log(data);
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-        console.log("Response text:", text);
-      }
+    .then((data) => {
+      console.log("respuesta del servidor", data)
     })
     .catch((error) => {
-      console.error("Error during fetch:", error);
+      console.log("este es el error que ha ocurrido durante el fetch", error)
     });
+  };
 
-    console.log("botón submit", "Clicked");
-  }
+  //Hago un watch a la contraseña para compararla con la confirmación
+  // watch es para observar campos
+
+  const password = watch("password")
+
 
   return (
     <div className="h-screen min-w-180 flex items-center justify-center bg-gray-100 overflow-auto">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-4xl">
         <h1 className="mb-4 text-4xl min-w-64 font-semibold text-center">Regístrese en nuestra página</h1>
 
-        <form onSubmit={handleForm}>
+        {/* Ahora paso al handleSubmit y a la función onSubmit a form */}
+
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-wrap -mx-4">
+
             <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Nombre</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
-                <input
-                  className="flex-1 outline-none bg-transparent"
-                  type="text"
-                  name="name"
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Escriba su nombre"
+                <input 
+                className="flex-1 outline-none bg-transparent"
+                {...register("name", {required: "El nombre es obligatorio"})} // registro del input con validación
+                placeholder="Escriba su nombre"
                 />
-                {console.log("Este es el nombre de usuario", name)}
               </div>
+              {errors.name && <p className="text-red-500">{errors.name.message}</p>}
             </div>
 
             <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Apellido</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
-                <input
-                  className="flex-1 outline-none bg-transparent"
-                  type="text"
-                  name="lastName"
-                  onChange={(e) => setLastName(e.target.value)}
-                  placeholder="Escriba su apellido"
+                <input 
+                className="flex-1 outline-none bg-transparent"
+                {...register("lastName", {required: "El apellido es obligatorio"})} // registro del input con validación
+                placeholder="Escriba su apellido"
                 />
-                {console.log("este es el apellido del usuario", lastName)}
               </div>
+              {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
             </div>
-
+          
             <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Edad</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
-                <input
-                  className="flex-1 outline-none bg-transparent"
-                  type="number"
-                  name="age"
-                  onChange={(e) => setAge(e.target.value)}
-                  placeholder="Escriba su edad"
+                <input 
+                className="flex-1 outline-none bg-transparent"
+                type="number"
+                {...register("age", {required: "La edad es obligatoria",
+                  min: {
+                    value: 18, 
+                    message: "Debes tener al menos 18 años para registrarte"
+                  }, 
+                  max: {
+                    value: 99, 
+                    message: "La edad máxima para registrarte son 99 años"
+                  }
+                })} // registro del input con validación
+                placeholder="Escriba su edad"
                 />
-                {console.log("Esta es la edad ", age)}
               </div>
+              {errors.age && <p className="text-red-500">{errors.age.message}</p>}
             </div>
 
             <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Email</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
-                <input
-                  className="flex-1 outline-none bg-transparent"
-                  type="email"
-                  name="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Escriba su email"
+                <input 
+                className="flex-1 outline-none bg-transparent"
+                type="email"
+                {...register("email", 
+                  {required: "El email es oligatorio", 
+                  pattern: { value: /^\S+@\S+$/i, message: "El email no es válido" },
+                  })} 
+                placeholder="Escriba su email"
                 />
-                {console.log("este es el email del usuario", email)}
               </div>
+              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
 
             <div className="w-full md:w-1/2 px-4 mb-4">
-              <label className="block mb-2 text-xl font-medium text-gray-700">NIF o CIF</label>
+              <label className="block mb-2 text-xl font-medium text-gray-700">NIE o NIF</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
                 <input
                   className="flex-1 outline-none bg-transparent"
-                  type="text"
-                  name="nif"
-                  onChange={(e) => setNif(e.target.value)}
-                  placeholder="Escriba su NIF o CIF"
+                  type="number" // Especifico que este campo es numérico
+                  {...register("nif", {
+                    required: "El NIE o NIF es obligatorio",
+                    minLength: { value: 7, message: "El NIE o NIF debe tener al menos 7 caracteres" },
+                    maxLength: { value: 9, message: "El NIE o NIF no puede exceder los 9 caracteres" },
+                  })}
+                  placeholder="Escriba su NIE o NIF"
                 />
-                {console.log("Este es el nif del usuario", nif)}
               </div>
-            </div>
+              {errors.nif && <p className="text-red-500">{errors.nif.message}</p>}
+            </div>        
+
 
             <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Contraseña</label>
               <div className="flex items-center border border-blue-600 p-2 rounded">
                 <input
                   className="flex-1 outline-none bg-transparent"
-                  type="text"
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  {...register("password", {
+                    required: "La contraseña es obligatoria",
+                    minLength: { value: 6, message: "La contraseña debe tener al menos 6 caracteres" },
+                  })}
                   placeholder="Escriba su contraseña"
                 />
-                {console.log("Esta es la contraseña", password)}
               </div>
+              {errors.password && <p className="text-red-500">{errors.password.message}</p>}
             </div>
 
-            <div className="w-full px-4 mb-4 relative">
+            <div className="w-full md:w-1/2 px-4 mb-4">
+              <label className="block mb-2 text-xl font-medium text-gray-700">Confirmar Contraseña</label>
+              <div className="flex items-center border border-blue-600 p-2 rounded">
+                <input
+                  className="flex-1 outline-none bg-transparent"
+                  type="password"
+                  {...register("confirmPassword", {
+                    required: "Debes confirmar tu contraseña",
+                    validate: (value) =>
+                      value === password || "Las contraseñas no coinciden", // Validación personalizada
+                  })}
+                  placeholder="Confirma tu contraseña"
+                />
+              </div>
+              {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
+            </div>
+
+            <div className="w-full md:w-1/2 px-4 mb-4">
               <label className="block mb-2 text-xl font-medium text-gray-700">Seleccione su Rol</label>
               <select
                 className="border border-blue-600 p-2 rounded w-full bg-white text-gray-400 appearance-none pr-10"
-                name="role"
+                {...register("role", { required: "El rol es obligatorio" })}
                 onChange={(e) => setRole(e.target.value)}
-                placeholder="Seleccione su Rol"
               >
                 <option value="" disabled hidden>
                   Seleccione su Rol
@@ -160,16 +178,7 @@ export const RegisterUser = () => {
                 <option value="admin">Admin</option>
                 <option value="user">User</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-4 right-2 flex items-center px-2 text-gray-700 h-full">
-                <svg
-                  className="fill-current h-6 w-6 mx-auto"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M7 10l5 5 5-5H7z" />
-                </svg>
-              </div>
-              {console.log("este es el rol seleccionado", role)}
+              {errors.role && <p className="text-red-500">{errors.role.message}</p>}
             </div>
           </div>
 
@@ -179,14 +188,13 @@ export const RegisterUser = () => {
             value="Registrar"
           />
           <div className="text-black mt-4 text-center text-sm p-6">
-              <p>¿Ya tienes cuenta? <Link to="/login" className="text-blue-600 cursor-pointer">Login</Link></p>
-              <br />
-              <p>Más información en <Link to="/" className="text-blue-600 cursor-pointer">Home</Link></p>
-            </div>
+            <p>¿Ya tienes cuenta? <Link to="/login" className="text-blue-600 cursor-pointer">Login</Link></p>
+            <br />
+            <p>Más información en <Link to="/" className="text-blue-600 cursor-pointer">Home</Link></p>
+          </div>
         </form>
       </div>
     </div>
   );
 };
-
 export default RegisterUser;
