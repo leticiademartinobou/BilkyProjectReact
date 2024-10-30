f9f2eb4054bd0c7f17e87e7b73151dbfd516453e
Leticia de Martino <leticiademartino@GREENWICH.local>
2024-10-30 16:56:07 +0100

    update 30.10.2024

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UpdateUserForm from './UpdateUserForm';
 
 
@@ -8,6 +9,8 @@ export const UserProfile = ({ name, lastName, role}) =>{
 
 const [documents, setDocuments] = useState([])
 const [error, setError] = useState(null)
+const [uploadFile, setUploadFile] = useState(null)
+const navigate = useNavigate()
 
 useEffect(() => {
 
@@ -18,7 +21,8 @@ useEffect(() => {
   
     if(!token) {
       console.log("El token no está en el localStorage")
-      return setError("El token no está en el LocalStorage, haz log in otra vez")
+      setError("El token no está en el LocalStorage, haz log in otra vez")
+      return navigate("/")
     }
   
     const headers = {
@@ -35,7 +39,13 @@ useEffect(() => {
       const data = await fetchResponse.json();
     
       if (!fetchResponse.ok) {
-        const data = await fetchResponse.json();
+        // const data = await fetchResponse.json();
+
+        //si el token está expirado o no es válido quiero que haga navigate al inicio.
+        if(fetchResponse.status === 401){ // 401 es el código de error
+          localStorage.removeItem("token"); // se borra el token inválido
+          return navigate("/")
+          }
 
           throw new Error(data.message || 'La respuesta del fetch no es correcta');
         }
@@ -59,7 +69,7 @@ useEffect(() => {
     }
   }
     fetchUserProfile()
-  }, [])
+  }, [navigate])
 
 const handleUpdate = async () => {
  const token = localStorage.getItem("token")
@@ -86,6 +96,45 @@ const handleUpdate = async () => {
 const handleDelete = () => {
  // poner la lógica del delete
 }
+
+const handleFileChange = (e) => {
+  setUploadFile(e.target.files[0])
+}
+
+const handleUpload = async () => {
+
+  const formData = new FormData();
+
+  formData.append("file", selectedFile)
+  formData.append("userId", /* aquí hay que poner el userId del usuario al que se le asigna el documento*/)
+
+  const token = localStorage.getItem("token")
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_APP_URL}/document/upload`, {
+      method: "POST", 
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body: formData
+    });
+
+    const data = await response.json()
+    if(data.success) {
+      alert("Documento añadido a tu carpeta virtual")
+      // aquí quiero que se vea el doc
+    } else {
+      alert(data.message || "error al subir el documento, compruebe el documento (tamaño, extensión) y pruebe d nuevo")
+    }
+
+  } catch (error) {
+    setError("error al subir el documento")
+    console.log("este es el error que hay al intentar subir el documento", error)
+  }
+
+  if(role !== "admin") return
+
+}
   
 return (
   <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg bg-gray-100">
@@ -108,11 +157,21 @@ return (
       ) : (
         <p>El usuario no tiene ningún documento</p>
       )}
+
+      {role === "admin" && (
+        <div className='mt-6 flex flex-col items-center gap-4'>
+          <input type='file' onChange={handleFileChange}/>
+          <button onClick={handleUpload} className='bg-blue-600 text-white px-4 py-2 mt-2 rounded'>
+            Upload Documento
+          </button>
+        </div>
+      )}
+
       {role === "admin" && <UpdateUserForm />}
 
       </div>
-      </div>
-);
+    </div>
+  );
 };
 
 export default UserProfile
